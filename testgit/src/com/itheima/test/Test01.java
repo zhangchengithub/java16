61bd325f12b69e544efc24be8b3cabe43b9e438e
zhangchen <15621588621@163.com>
2017-09-12 21:36:26 +0800

    tianjai  yige fangf

--- testgit/src/com/itheima/test/Test01.java
+++ testgit/src/com/itheima/test/Test01.java
@@ -5,4 +5,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("000000000000000000");
 	}
+	public static void main2(String[] args) {
+		System.out.println("000000000000000000");
+	}
 }
