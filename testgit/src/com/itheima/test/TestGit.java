a63344a647a304c9cfd5f5eedfebb9053b7bfbaf
zhangchen <15621588621@163.com>
2017-09-12 21:45:59 +0800

    添加

--- testgit/src/com/itheima/test/TestGit.java
+++ testgit/src/com/itheima/test/TestGit.java
@@ -5,4 +5,13 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("helloWorld");
 	}
+	public static void main1(String[] args) {
+		System.out.println("helloWorld");
+	}
+	public static void main2(String[] args) {
+		System.out.println("helloWorld");
+	}
+	public static void main4(String[] args) {
+		System.out.println("helloWorld");
+	}
 }
